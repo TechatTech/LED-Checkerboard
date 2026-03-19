fc84f52d013c3373233243652b115304811c18cc
Leslie Crowson <llcrowsonIV@yahoo.com>
2026-03-19 14:54:57 -0500

    initial version of the testing program

---
+++ Testing testing.py
@@ -0,0 +1,16 @@
+def student1Function():
+    print("Hello this is Lars")
+    
+    pass
+
+def student2Function():
+    
+    pass
+
+def student3Function():
+
+    pass
+############# main program ##########
+student1Function()
+student2Function()
+student3Function()
